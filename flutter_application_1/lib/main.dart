5fb2a208f7a4476df9ea92c070218aedb112c2c2
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2025-08-27 21:14:50 -0500

    consumiendo imagenes

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
 
-void main() {
-  runApp(const App());
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Imagenes",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Mostrar imagenes")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 250.0,
+                width: 550.0,
+                child: Image.asset(
+                  "elpandacreador_profile.jpg",
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              SizedBox(
+                height: 250.0,
+                width: 550.0,
+                child: Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG",
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
