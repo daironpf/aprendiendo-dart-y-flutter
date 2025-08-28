fa86dcb90ed3d2255afa2db6c9d1c11d15d6a92f
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2025-08-27 20:22:50 -0500

    Terminado el ejemplo del contador

--- flutter_application_1/lib/src/app.dart
+++ flutter_application_1/lib/src/app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import './pages/home.dart';
+import './pages/contador.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Center(child: Home()));
+    return const MaterialApp(home: Center(child: Contador()));
   }
 }
--- flutter_application_1/lib/src/pages/contador.dart
+++ flutter_application_1/lib/src/pages/contador.dart
@@ -1,1 +1,39 @@
 import 'package:flutter/material.dart';
+
+//clase donde se redibuja el widget segun los cambios
+class Contador extends StatefulWidget {
+  const Contador({super.key});
+
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+//Clase donde escucha los cambios
+class _ContadorState extends State<Contador> {
+  int _cont = 25;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Contador")),
+      body: Center(
+        child: Text('Contador $_cont', style: TextStyle(fontSize: 20)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _incrementar();
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _incrementar() {
+    setState(() {
+      //toda accion de modificacion de informacion o estado de variables, se debe poner aqui dentro para que se actualice en caliente.
+      _cont++;
+    });
+  }
+}
