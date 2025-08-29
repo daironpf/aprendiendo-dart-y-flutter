59c022c011c2c96ac17fec8e6fe25dcd72eab240
Dairon Perez Frias <dairon.perezfrias@gmail.com>
2025-08-29 13:54:28 -0500

    un paso

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/ContainerPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(title: "Container Widget", home: Containerpage());
   }
 }
