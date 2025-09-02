9915c232e4083034a56a5bffcaebe17c57e6d525
Dairon Perez Frias <dairon.perezfrias@gmail.com>
2025-09-02 07:18:09 -0500

    ListView con builder

--- flutter_application_1/lib/pages/ListView.dart
+++ flutter_application_1/lib/pages/ListView.dart
@@ -8,16 +8,37 @@ class ListviewPage extends StatefulWidget {
 }
 
 class _ListviewPageState extends State<ListviewPage> {
+  final List<String> nombres = <String>[
+    'Ana',
+    'Juan',
+    'Pedro',
+    'Maria',
+    'Luis',
+    'Carmen',
+    'Jose',
+    'Lucia',
+    'Miguel',
+    'Sofia',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("ListView")),
-      body: ListView(
-        children: <Widget>[
-          Container(height: 50, color: Colors.red),
-          Container(height: 50, color: Colors.amber),
-          Container(height: 50, color: Colors.blue),
-        ],
+      body: ListView.builder(
+        itemCount: nombres.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: const Icon(Icons.person),
+            title: Text(nombres[index]),
+            subtitle: const Text("Cualquier cosa"),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              // ignore: avoid_print
+              print("Hola $index");
+            },
+          );
+        },
       ),
     );
   }
