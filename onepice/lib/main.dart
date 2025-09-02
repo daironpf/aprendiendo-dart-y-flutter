de9d33bb3d8565422a8750f59378107cbfa92e7f
Dairon Perez Frias <dairon.perezfrias@gmail.com>
2025-09-02 09:34:01 -0500

    iniciada las clases

--- onepice/lib/main.dart
+++ onepice/lib/main.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:onepice/src/pages/home_page.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "One Piece List", home: Home());
+  }
+}
