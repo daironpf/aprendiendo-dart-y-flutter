4f6bce0e7407747b38ca6f011f453371a7eaeb21
Dairon Perez Frias <dairon.perezfrias@gmail.com>
2025-09-05 21:12:02 -0500

    agregado el boton de buscar

--- onepice/lib/src/widgets/header_widget.dart
+++ onepice/lib/src/widgets/header_widget.dart
@@ -2,11 +2,48 @@ import 'package:flutter/material.dart';
 
 class HeaderWidget extends StatelessWidget {
   const HeaderWidget({super.key});
+  final String title = "One Piece";
+  final tituloTextStyle = const TextStyle(
+    color: Colors.white,
+    fontSize: 26.0,
+    fontWeight: FontWeight.bold,
+  );
+  final subtitleStyle = const TextStyle(color: Colors.white, fontSize: 17.0);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: ,
+      alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.all(25.0),
+      height: 170.0,
+      decoration: BoxDecoration(color: Color.fromARGB(255, 41, 40, 39)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: tituloTextStyle),
+              Text("Series", style: subtitleStyle),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 73, 66, 66),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.search, size: 28, color: Colors.white),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
